4c7fb29ec0a8f8d3d9a580e0adadbf1d417d4716
Danish Suhail <danish_suhail@outlook.com>
2024-11-24 03:54:34 +0530

    v1.0.2

--- scripts/backend/main.js
+++ scripts/backend/main.js
@@ -18,18 +18,21 @@ function FilterData(data,option1,option2) {
 const option1 = document.getElementById('option1');
 const option2 = document.getElementById('option2');
 
+// setting default values
+localStorage.setItem("departure","New Delhi");
+localStorage.setItem("arrival","Mumbai"); 
+localStorage.setItem("selectedKey","None");
+
 option1.addEventListener('change', async () => {
     localStorage.setItem("departure",option1.value);
     localStorage.setItem("arrival",option2.value);
     const data = await fetchData();
-    localStorage.setItem("selectedKey","None");
     FilterData(data,option1.value,option2.value);
 });
   
 option2.addEventListener('change', async () => {
     localStorage.setItem("departure",option1.value);
     localStorage.setItem("arrival",option2.value);
-    localStorage.setItem("selectedKey","None");
     const data = await fetchData();
     FilterData(data,option1.value,option2.value);
 });
